c8a8984e03fbf2881b22d8d92a39d7460f603bce
Dhwani3696 <ravaldhwani9423@gmail.com>
2017-05-30 15:13:51 +0530

    first commit

---
+++ app/src/main/java/com/example/dhwani_android/gallery_view/GalleryImageAdapter.java
@@ -0,0 +1,62 @@
+package com.example.dhwani_android.gallery_view;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+/**
+ * Created by DHWANI-ANDROID on 30-05-17.
+ */
+
+public class GalleryImageAdapter extends BaseAdapter
+{
+    private Context mContext;
+
+
+
+    public GalleryImageAdapter(Context context)
+    {
+        mContext = context;
+    }
+
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    // Override this method according to your need
+    public View getView(int index, View view, ViewGroup viewGroup)
+    {
+        // TODO Auto-generated method stub
+        ImageView i = new ImageView(mContext);
+
+        i.setImageResource(mImageIds[index]);
+        i.setLayoutParams(new Gallery.LayoutParams(200, 200));
+
+        i.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        return i;
+    }
+
+    public Integer[] mImageIds = {
+            R.drawable.i9,
+            R.drawable.i2,
+            R.drawable.i3,
+            R.drawable.i4,
+            R.drawable.i5,
+            R.drawable.i6,
+            R.drawable.i7,
+            R.drawable.i8
+    };
+}
